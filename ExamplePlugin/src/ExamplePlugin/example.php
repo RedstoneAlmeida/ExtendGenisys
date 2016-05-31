20d5c8a3092c42f28fb65585cf45ca368e29bab6
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-05-31 18:38:12 -0300

    Add files via upload

---
+++ ExamplePlugin/src/ExamplePlugin/example.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ExamplePlugin;
+
+use pocketmine\plugin\PluginBase as PlgBase;
+use pocketmine\event\Listener as ListN;
+use pocketmine\Player;
+
+use pocketmine\item\Item;
+
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerItemHeldEvent;
+
+class example extends PlgBase implements ListN{
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    
+    public function PlayerDrop(PlayerDropItemEvent $event){
+        if($event->getPlayer()->getInventory()->getItem() == 264){
+            $event->setCancelled(true);
+        }
+    }
+    
+}
+
